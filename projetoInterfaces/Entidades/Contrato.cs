eaba01010da5bc7dc272127a3a2229ce1739ca87
Vagner Alves <vagneralvesgomes@gmail.com>
2018-12-08 20:20:22 -0200

    Comecei a construcao da classe ServicoContrato

--- projetoInterfaces/Entidades/Contrato.cs
+++ projetoInterfaces/Entidades/Contrato.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace projetoInterfaces.Dominio
+namespace projetoInterfaces.Entidades
 {
 	class Contrato
 	{
---
+++ projetoInterfaces/Servicos/ServicoContrato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using projetoInterfaces.Entidades;
+
+namespace projetoInterfaces.Servicos
+{
+	class ServicoContrato
+	{
+		public Contrato Cont { get; set; }
+		public int Meses { get; set; }
+		private ServicoPaypal sp;
+
+		public ServicoContrato(Contrato cont, int meses)
+		{
+			Cont = cont;
+			Meses = meses;
+		}
+
+		public void ProcessarContrato(Contrato c, int meses)
+		{
+			
+		}
+	}
+}
